44f4577f13758dbf3f3efb34061b55a687e355f7
Artur Luna <90049760+manguetownson@users.noreply.github.com>
2023-09-27 17:49:42 +0000

    criação do histórico das jogadas

---
+++ jogo-de-dados-native/src/components/History.js
@@ -0,0 +1,48 @@
+// History.js
+import React, { useEffect } from 'react';
+import { SectionList, Text, View } from 'react-native';
+import { useStore } from './store'; // Importe o hook do Zustand
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const History = () => {
+  const { history, addToHistory, clearHistory } = useStore(); // Use o estado e funções de atualização do Zustand
+
+  useEffect(() => {
+    // Carregar o histórico do AsyncStorage quando o componente é montado
+    loadHistory();
+  }, []);
+
+  const loadHistory = async () => {
+    try {
+      const savedHistory = await AsyncStorage.getItem('history');
+      if (savedHistory !== null) {
+        addToHistory(JSON.parse(savedHistory));
+      }
+    } catch (error) {
+      console.error('Erro ao carregar o histórico:', error);
+    }
+  };
+
+  const clearHistoryHandler = () => {
+    clearHistory();
+    AsyncStorage.removeItem('history');
+  };
+
+  return (
+    <View>
+      <SectionList
+        sections={history}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => <Text>{item}</Text>}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={{ fontWeight: 'bold' }}>{title}</Text>
+        )}
+      />
+      <View style={{ marginTop: 20 }}>
+        <Button title="Limpar Histórico" onPress={clearHistoryHandler} />
+      </View>
+    </View>
+  );
+};
+
+export default History;
