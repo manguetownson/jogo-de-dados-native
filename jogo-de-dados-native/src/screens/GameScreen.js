63c8251e9982ba76671c497d8b3d1f033f6d27fe
Artur Luna <90049760+manguetownson@users.noreply.github.com>
2023-09-27 17:50:01 +0000

    criação da tela do jogo

---
+++ jogo-de-dados-native/src/screens/GameScreen.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, Button, Modal } from 'react-native';
+import Game from './Game';
+import HistoryScreen from './HistoryScreen'; // Você pode integrar o histórico aqui
+
+
+const GameScreen = () => {
+    const [isHistoryVisible, setIsHistoryVisible] = useState(false);
+    const [historyData, setHistoryData] = useState([]);
+  
+    // Função para mostrar o modal de histórico
+    const showHistoryModal = () => {
+      setIsHistoryVisible(true);
+    };
+  
+    // Função para esconder o modal de histórico
+    const hideHistoryModal = () => {
+      setIsHistoryVisible(false);
+    };
+    
+    // Função para adicionar uma entrada no histórico
+    const addToHistory = (entry) => {
+      setHistoryData([...historyData, entry]);
+    };
+  
+    return (
+      <View style={styles.container}>
+        <Game addToHistory={addToHistory} /> {/* Passa a função addToHistory como propriedade para o componente do jogo */}
+        <Button title="Ver Histórico" onPress={showHistoryModal} />
+        
+        {/* Modal para exibir o histórico */}
+        <Modal
+          visible={isHistoryVisible}
+          animationType="slide"
+          transparent={false}
+          onRequestClose={hideHistoryModal}
+        >
+          <View style={styles.modalContainer}>
+            <Button title="Fechar" onPress={hideHistoryModal} />
+            <HistoryScreen data={historyData} /> {/* Passa o histórico como propriedade para o componente de histórico */}
+          </View>
+        </Modal>
+      </View>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 20,
+    },
+    modalContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 22,
+    },
+  });
+  
+  export default GameScreen;
+  
